3665e4de74e70ff6d2d37eb87f7cfbcee97a3882
Christopher Tran <pfftdammitchris@gmail.com>
2019-11-13 21:22:08 -0800

    Init

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
 import React from "react";
+import HomeIcon from "@material-ui/icons/Home";
+import ReceiptIcon from "@material-ui/icons/Receipt";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import DesktopWindowsIcon from "@material-ui/icons/DesktopWindows";
+import SettingsIcon from "@material-ui/icons/Settings";
 import Sidebar from "./Sidebar";
 
+function onClick(e, item) {
+  window.alert(JSON.stringify(item, null, 2));
+}
+
 const items = [
-  { name: "home", label: "Home" },
+  { name: "home", label: "Home", Icon: HomeIcon },
   {
     name: "billing",
     label: "Billing",
+    Icon: ReceiptIcon,
     items: [
-      { name: "statements", label: "Statements" },
-      { name: "reports", label: "Reports" }
+      { name: "statements", label: "Statements", onClick },
+      { name: "reports", label: "Reports", onClick }
     ]
   },
+  "divider",
   {
     name: "settings",
     label: "Settings",
-    items: [{ name: "profile", label: "Profile" }]
+    Icon: SettingsIcon,
+    items: [
+      { name: "profile", label: "Profile" },
+      { name: "insurance", label: "Insurance", onClick },
+      "divider",
+      {
+        name: "notifications",
+        label: "Notifications",
+        Icon: NotificationsIcon,
+        items: [
+          { name: "email", label: "Email", onClick },
+          {
+            name: "desktop",
+            label: "Desktop",
+            Icon: DesktopWindowsIcon,
+            items: [
+              { name: "schedule", label: "Schedule" },
+              { name: "frequency", label: "Frequency" }
+            ]
+          },
+          { name: "sms", label: "SMS" }
+        ]
+      }
+    ]
   }
 ];
 
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,51 +1,101 @@
 import React from "react";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import Collapse from "@material-ui/core/Collapse";
 
-function SidebarItem({ name, label }) {
+function SidebarItem({ depthStep = 10, depth = 0, expanded, item, ...rest }) {
+  const [collapsed, setCollapsed] = React.useState(true);
+  const { label, items, Icon, onClick: onClickProp } = item;
+
+  function toggleCollapse() {
+    setCollapsed(prevValue => !prevValue);
+  }
+
+  function onClick(e) {
+    if (Array.isArray(items)) {
+      toggleCollapse();
+    }
+    if (onClickProp) {
+      onClickProp(e, item);
+    }
+  }
+
+  let expandIcon;
+
+  if (Array.isArray(items) && items.length) {
+    expandIcon = !collapsed ? (
+      <ExpandLessIcon
+        className={
+          "sidebar-item-expand-arrow" + " sidebar-item-expand-arrow-expanded"
+        }
+      />
+    ) : (
+      <ExpandMoreIcon className="sidebar-item-expand-arrow" />
+    );
+  }
+
   return (
-    <ListItem key={name} style={{ paddingLeft: 36 }} button dense>
-      <ListItemText>
-        <span className="sidebar-subitem-text">{label}</span>
-      </ListItemText>
-    </ListItem>
+    <>
+      <ListItem
+        className="sidebar-item"
+        onClick={onClick}
+        button
+        dense
+        {...rest}
+      >
+        <div
+          style={{ paddingLeft: depth * depthStep }}
+          className="sidebar-item-content"
+        >
+          {Icon && <Icon className="sidebar-item-icon" fontSize="small" />}
+          <div className="sidebar-item-text">{label}</div>
+        </div>
+        {expandIcon}
+      </ListItem>
+      <Collapse in={!collapsed} timeout="auto" unmountOnExit>
+        {Array.isArray(items) ? (
+          <List disablePadding dense>
+            {items.map((subItem, index) => (
+              <React.Fragment key={`${subItem.name}${index}`}>
+                {subItem === "divider" ? (
+                  <Divider style={{ margin: "6px 0" }} />
+                ) : (
+                  <SidebarItem
+                    depth={depth + 1}
+                    depthStep={depthStep}
+                    item={subItem}
+                  />
+                )}
+              </React.Fragment>
+            ))}
+          </List>
+        ) : null}
+      </Collapse>
+    </>
   );
 }
 
-function Sidebar({ items }) {
+function Sidebar({ items, depthStep, depth, expanded }) {
   return (
     <div className="sidebar">
       <List disablePadding dense>
-        {items.map(({ label, name, items: subItems, ...rest }) => {
-          return (
-            <React.Fragment key={name}>
-              <ListItem style={{ paddingLeft: 18 }} button {...rest}>
-                <ListItemText>{label}</ListItemText>
-              </ListItem>
-              {Array.isArray(subItems) ? (
-                <List disablePadding dense>
-                  {subItems.map(subItem => {
-                    return (
-                      <ListItem
-                        key={subItem.name}
-                        style={{ paddingLeft: 36 }}
-                        button
-                        dense
-                      >
-                        <ListItemText>
-                          <span className="sidebar-subitem-text">
-                            {subItem.label}
-                          </span>
-                        </ListItemText>
-                      </ListItem>
-                    );
-                  })}
-                </List>
-              ) : null}
-            </React.Fragment>
-          );
-        })}
+        {items.map((sidebarItem, index) => (
+          <React.Fragment key={`${sidebarItem.name}${index}`}>
+            {sidebarItem === "divider" ? (
+              <Divider style={{ margin: "6px 0" }} />
+            ) : (
+              <SidebarItem
+                depthStep={depthStep}
+                depth={depth}
+                expanded={expanded}
+                item={sidebarItem}
+              />
+            )}
+          </React.Fragment>
+        ))}
       </List>
     </div>
   );
